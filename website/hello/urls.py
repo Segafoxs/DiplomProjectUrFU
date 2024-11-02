20239f1a11d81010b97458faba01710ffec42968
Segafox <segafox2@mail.ru>
2024-11-02 16:18:12 +0500

    Создана страница авторизации

--- website/hello/urls.py
+++ website/hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("page", views.page, name = "page")
+    path("welcomePage", views.first_page, name="firstPage"),
+    path("workPermit", views.work_permit, name = "workPermit")
 ]
--- website/hello/views.py
+++ website/hello/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'hello/index.html')
+    return render(request, 'hello/authorization/index.html')
 
-def page(request):
-    return render(request, 'hello/firstPage.html')
+def first_page(request):
+    return render(request, 'hello/firstPage/firstPage.html')
+
+def work_permit(request):
+    return render(request, 'hello/workPermit/workPermit.html')
+
