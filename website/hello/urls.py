49c1937948d8cbe5df7d3c5cfebe3fe1cd0d7d9a
Segafox <segafox2@mail.ru>
2024-11-13 21:56:18 +0500

    Добавлен скрипт для вставления записей в Ворд, после отправки формы на сервер

--- website/hello/urls.py
+++ website/hello/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("welcomePage", views.first_page, name="welcomePage"),
-    path("workPermit", views.work_permit, name="workPermit"),
-    path("currentPermit", views.current_permit, name="currentPermit")
+    path("welcomePage/", views.first_page, name="welcomePage"),
+    path("workPermit/", views.work_permit, name="workPermit"),
+    path("currentPermit/", views.current_permit, name="currentPermit"),
+    path("workPermit/postuser/", views.postuser),
 ]
--- website/hello/views.py
+++ website/hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from docxtpl import DocxTemplate
 
 def index(request):
     return render(request, 'hello/authorization/index.html')
@@ -13,3 +14,75 @@ def work_permit(request):
 
 def current_permit(request):
     return render(request, 'hello/currentWorkPermits/currentWork.html')
+
+
+def insert_into_doc(manager, managerPost, executor, executorPost,
+                    countMember, member, work, dateStart, timeStart, dateEnd,
+                    timeEnd, conditions, director, directorPost,
+                    personal, personalPost):
+    doc = DocxTemplate("C:\\Users\\Сергей\\Desktop\\диплом\\test.docx")
+    context = {
+                'manager': manager,
+                'managerPost': managerPost,
+                'executor': executor,
+                'executorPost': executorPost,
+                'countMember': countMember,
+                'member': member,
+                'work': work,
+                'dateStart': dateStart,
+                'timeStart': timeStart,
+                'dateEnd': dateEnd,
+                'timeEnd': timeEnd,
+                'conditions': conditions,
+                'director': director,
+                'directorPost': directorPost,
+                'personal': personal,
+                'personalPost': personalPost
+    }
+    doc.render(context)
+    doc.save("final.docx")
+    return {"message": "sucessy"}
+
+
+
+
+def postuser(request):
+    # получаем из данных запроса POST отправленные через форму данные
+
+    #Можно сделать класс Руководитель, производитель
+    manager = request.POST.get("manager")
+    managerPost = request.POST.get("managerPost")
+
+    #Производитель
+    executor = request.POST.get("executor")
+    executorPost = request.POST.get("executorPost")
+
+    #Член бригады
+    countMember = request.POST.get("countMember")
+    member = request.POST.get("member")
+
+    # #Работа
+    work = request.POST.get("work")
+    dateStart = request.POST.get("dateStart")
+    timeStart = request.POST.get("timeStart")
+
+    dateEnd = request.POST.get("dateEnd")
+    timeEnd = request.POST.get("timeEnd")
+
+    conditions = request.POST.get("conditions")
+
+    director = request.POST.get("director")
+    directorPost = request.POST.get("directorPost")
+
+    personal = request.POST.get("personal")
+    personalPost = request.POST.get("personalPost")
+
+
+    insert_into_doc(manager, managerPost, executor, executorPost,
+                    countMember, member, work, dateStart, timeStart, dateEnd,
+                    timeEnd, conditions, director, directorPost,
+                    personal, personalPost)
+
+    return HttpResponse(f'Руководитель: {manager}, Производитель: {executor}, '
+                        f'Количество членов бригады: {countMember}, Члены бригады: {member},'
+                        f'{work}')
